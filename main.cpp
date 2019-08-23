7bb82ae802fe12be3ac6d87d0d3ebe8c1370819b
TheGreatCookieMachine <39784551+TheGreatCookieMachine@users.noreply.github.com>
2019-08-23 16:53:11 -0700

    Added Snake Movement

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <array>
+#include <chrono>
 
 // Doesn't count walls, actual screen size is 2 higher than inputted value
 const int SCREEN_HEIGHT = 32;
@@ -32,6 +33,30 @@ public:
         this->body.push_back(position);
         this->direction = direction;
     }
+
+    void process() {
+        this->body.push_back(this->body.back());
+        for (int i = this->body.size() - 1; i > 0; i--) {
+            this->body[i] = this->body[i-1];
+        }
+
+        std::array<int, 2> destination = this->body[1];
+        switch (this->direction) {
+        case up:
+            destination[0]--;
+            break;
+        case down:
+            destination[0]++;
+            break;
+        case left:
+            destination[1]--;
+            break;
+        case right:
+            destination[1]++;
+            break;
+        }
+        this->body[0] = destination;
+    }
 };
 
 class Game {
@@ -43,6 +68,10 @@ public:
         clearBoard();
     }
 
+    void process() {
+        this->snake.process();
+    }
+
     void clearBoard() {
         for (int i = 0; i < SCREEN_HEIGHT; i++) {
             for (int j = 0; j < SCREEN_WIDTH; j++) {
@@ -52,6 +81,8 @@ public:
     }
 
     void printGame() {
+        std::system("cls");
+
         for (std::array<int, 2> position: this->snake.body) {
             this->screen[position[0]][position[1]] = SNAKE_CHAR;
         }
@@ -83,6 +114,9 @@ public:
 int main()
 {
     Game game = Game({5, 5}, up);
+    game.printGame();
+    _sleep(1000);
+    game.process();
     game.printGame();
     return 0;
 }
